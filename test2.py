21f02d92ac8958f9062c7ae9f5d7cb40c39c035f
Najeeb Aqel <najeebaqel@ieee.org>
2019-03-24 18:03:37 +0300

    Second commit

--- test2.py
+++ test2.py
@@ -29,12 +29,15 @@ for c in cnts:
 	# bounding box on both input images to represent where the two
 	# images differ
 	(x, y, w, h) = cv2.boundingRect(c)
+	cooX = (x+(w/2))
+	cooY = (y-(h/2))
+	print("coordination="+str(cooX)+","+str(cooY))
 	cv2.rectangle(imageA, (x, y), (x + w, y + h), (0, 0, 255), 2)
 	cv2.rectangle(imageB, (x, y), (x + w, y + h), (0, 0, 255), 2)
  
 # show the output images
-cv2.imshow("Original", imageA)
-cv2.imshow("Modified", imageB)
-cv2.imshow("Diff", diff)
-cv2.imshow("Thresh", thresh)
+cv2.imshow("input-left", imageA)
+cv2.imshow("input-right", imageB)
+cv2.imshow("Differential Gray Scale", diff)
+cv2.imshow("Differential Binary Scale", thresh)
 cv2.waitKey(0)
